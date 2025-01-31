fa4219a399c875b026f4839b230b8c68dfb2a9b5
Josep <jfalagan20987@iesjoanramis.org>
2025-01-31 17:40:09 +0100

    ud3 ex5

---
+++ ud3_ex5_josep_falagan/js/index.js
@@ -0,0 +1,6 @@
+const mobileMenuBtn = document.querySelector("#mobile-menu-btn");
+const navMenu = document.querySelector("#nav-menu");
+
+mobileMenuBtn.addEventListener("click", ()=>{
+  navMenu.classList.toggle("nav-visible");
+});
